0fbe6d25659ede4d81cc56213db312b5c1bcbcdb
Jhosselyn30 <astridjhosselyn@gmail.com>
2025-01-13 19:31:55 -0500

    Ultimas funcionalidades agregadas

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -20,16 +21,58 @@ export class LabsComponent {
   disable =true
   img='https://w3schools.com/howto/img_avatar.png';
 
-  person = {
+  person = signal({
     name: 'Jhimy',
-    age: 24,
+    age: 19,
     avatar: 'https://w3schools.com/howto/img_avatar.png'
+  })
+
+  ColorCtrl = new FormControl();
+
+  widthCtrl = new FormControl(50, {
+    nonNullable: true,
+  });
+
+  nameCtrl = new FormControl('Joss', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3) //tamaño min de un nombre (3char)
+    ]
+  });
+
+  constructor(){
+    this.ColorCtrl.valueChanges.subscribe(value => {
+      console.log(value);
+    })
   }
   //funciones (método)
   clickMe(){
     alert('Hola')
   }
+  changeName(event: Event){
+    const input =event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState =>{
+      return {
+        ...prevState,
+        name: newValue
+      }
+    });
+  }
+  newValue: number = 0; // Variable para almacenar el número
 
+  changeAge(event: Event){
+    const input =event.target as HTMLInputElement;
+    const newValue = input.value;
+
+    this.person.update(prevState =>{
+        return {
+          ...prevState,
+          age: parseInt(newValue)
+        }
+    } )
+  }
   changeHandler(event: Event){
     const input =event.target as HTMLInputElement;
     const newValue = input.value;
@@ -40,4 +83,7 @@ export class LabsComponent {
     const input = event.target as HTMLInputElement;
     console.log(input.value);
   }
+
+
+
 }
